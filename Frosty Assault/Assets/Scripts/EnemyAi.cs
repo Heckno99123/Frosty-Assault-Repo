dccad6195ef14effa4da2c2065be8d46523dae1e
Heckno99123 <426376@students.bethelsd.org>
2026-07-28 13:30:19 -0700

    AI basics

--- Frosty Assault/Assets/Scripts/EnemyAi.cs
+++ Frosty Assault/Assets/Scripts/EnemyAi.cs
@@ -4,24 +4,31 @@ using System.Collections.Generic;
 
 public class EnemyAi : MonoBehaviour
 {
-    public GameObject Chicken;
-    public GameObject Elves;
-    private float Enemy = 1.0f;
+    private string enemyType;
+    public GameObject player;
+    public float moveSpeed;
+    private float playerDistance;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-       
+        enemyType = gameObject.tag;
+        player = GameObject.Find("Player").transform.GetChild(0).gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
-    private void Awake()
-    {
-        Player = GameObject.Find("player").transform;
-        
-
+        playerDistance = Vector3.Distance(gameObject.transform.position, player.gameObject.transform.position);
+        if ( enemyType == "Chicken" )
+        {
+            if ( playerDistance <= 75 )
+            {
+                Vector3 directionToMove = player.transform.position - gameObject.transform.position;
+                directionToMove = directionToMove.normalized * Time.deltaTime * moveSpeed;
+                transform.position = gameObject.transform.position + Vector3.ClampMagnitude(directionToMove, moveSpeed);
+                
+            }
+        }
     }
 }
